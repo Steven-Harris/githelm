93fdbc3021b0f42c903261b55d910d4ae913d722
Steven Harris <steharris@jackhenry.com>
2024-12-26 01:14:13 -0600

    move jobs into workflowRun

--- src/components/actions/WorkflowRun.svelte
+++ src/components/actions/WorkflowRun.svelte
@@ -2,16 +2,41 @@
   import { fetchWorkflowJobs } from "../../services/github";
   import type { Job } from "../../services/models";
   import createPollingStore from "../../services/poll";
-  import Jobs from "./Jobs.svelte";
   let { org, repo, run } = $props();
   const jobsStore = createPollingStore<Job[]>(`${org}-${repo}-${run.id}-jobs`, () => fetchWorkflowJobs(org, repo, run.id));
+
+  const jobColor = (job: Job) => {
+    if (job.status === "completed" && job.conclusion === "success") {
+      return "bg-sky-600 border-sky-800 text-white";
+    } else if (job.conclusion === "failure") {
+      return "bg-red-200 border-red-600";
+    } else if (job.status === "waiting") {
+      return "bg-yellow-500 border-yellow-800";
+    } else if (job.status === "in_progress") {
+      return "bg-sky-300 border-sky-800";
+    } else {
+      return "bg-gray-300 border-gray-300";
+    }
+  };
 </script>
 
 <li class="mb-2 flex-grow items-center">
   <div class="cursor-pointer p-2 bg-gray-700 rounded-md hover:bg-gray-600 flex-grow">
     <a href={run.html_url} target="_blank" class="hover:underline">{run.display_title}</a>
     <ul class="flex flex-wrap -m-1">
-      <Jobs jobs={$jobsStore} />
+      {#each $jobsStore as job (job.id)}
+        <li class="p-1">
+          {#if job.status == "pending"}
+            <span class="{jobColor(job)} flex justify-between items-center">
+              <span class="text-sm">{job.name}</span>
+            </span>
+          {:else}
+            <button class="{jobColor(job)} cursor-pointer border rounded-lg p-3 shadow-sm flex justify-between items-center text-white">
+              {job.name}
+            </button>
+          {/if}
+        </li>
+      {/each}
     </ul>
   </div>
 </li>
