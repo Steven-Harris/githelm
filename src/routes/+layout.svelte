8efa375be4c54bf380ee22b8a2b378eefb163f9f
Steven Harris <steharris@jackhenry.com>
2025-07-02 21:35:25 -0500

    flip login spinner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -70,7 +70,13 @@
           class="flex items-center justify-center mx-auto bg-[#2ea043] hover:bg-[#3fb950] text-white font-medium px-6 py-3 rounded-md transition-all duration-200 shadow-lg hover:shadow-xl transform hover:translate-y-[-1px] active:translate-y-[1px]"
           onclick={login}
         >
-          {#if $isAuth !== 'authenticating'}
+          {#if $isAuth === 'authenticating'}
+            <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+              <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+            <span>Logging in...</span>
+          {:else}
             <svg class="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
               <path
                 fill-rule="evenodd"
@@ -79,12 +85,6 @@
               ></path>
             </svg>
             <span>Login with GitHub</span>
-          {:else}
-            <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-              <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-              <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
-            <span>Logging in...</span>
           {/if}
         </button>
       </div>
